dafa6648fd6e17a94608dda0113bb69356f858b6
queban <queban12@gmail.com>
2026-07-12 15:40:26 -0500

    Added ownership check to alert preferences

--- src/main/java/com/example/fabricalertengine/repository/AlertPreferenceRepository.java
+++ src/main/java/com/example/fabricalertengine/repository/AlertPreferenceRepository.java
@@ -4,6 +4,9 @@ import com.example.fabricalertengine.entity.AlertPreference;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AlertPreferenceRepository extends JpaRepository<AlertPreference, Long> {
+    List<AlertPreference> findByUserEmail(String email);
 }
