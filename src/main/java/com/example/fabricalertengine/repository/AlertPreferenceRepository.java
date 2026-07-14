c2f2b2ca7cc7021872db5c9e5ca048bb4cf472bd
queban <queban12@gmail.com>
2026-07-14 06:12:55 -0500

    AlertPreference Changes

--- src/main/java/com/example/fabricalertengine/repository/AlertPreferenceRepository.java
+++ src/main/java/com/example/fabricalertengine/repository/AlertPreferenceRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AlertPreferenceRepository extends JpaRepository<AlertPreference, Long> {
     List<AlertPreference> findByUserEmail(String email);
+    Optional<AlertPreference> findByUserEmailAndFabricId(String email, Long fabricId);
 }
